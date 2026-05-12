373302f9628292bca3b2c91d6cfdf6a6820023aa
lucas1noid <lucas1noid@gmail.com>
2026-05-12 13:45:50 -0300

    update: spring bootgit checkout -b spring

---
+++ src/test/java/com/meupet/MeupetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meupet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeupetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
